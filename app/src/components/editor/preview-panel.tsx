0a56526dd03cd2c56864df294ec9c77ee4db92ea
nicoledeancreative-commits <nicoledeancreative@gmail.com>
2026-07-28 10:41:20 -0400

    Stretch primary logo slot to full width and make placeholder background transparent

--- app/src/components/editor/preview-panel.tsx
+++ app/src/components/editor/preview-panel.tsx
@@ -81,7 +81,7 @@ export function PreviewPanel({
           minHeight: 400,
         }}
       >
-        <ImageSlot url={f.primaryLogoUrl} placeholder="Logo will appear here once uploaded" fit="contain" radius={10} readOnly style={{ width: 380, height: 380 }} />      </div>
+        <ImageSlot url={f.primaryLogoUrl} placeholder="Logo will appear here once uploaded" fit="contain" radius={10} readOnly style={{ width: "100%", height: 380 }} />      </div>
 
       {/* Brand Foundation */}
       <Sheet blockBg={blockBg}>
